24c5c498e9c301658e437eb95ed8b6244caaec33
Mario Donchev <mariodonchew@gmail.com>
2018-04-18 18:16:52 +0300

    Added interface for functionalities of user dao

---
+++ FinalProject/src/model/dao/IUserDao.java
@@ -0,0 +1,13 @@
+package model.dao;
+
+import java.sql.SQLException;
+
+import model.User;
+import util.exceptions.UserDataException;
+
+public interface IUserDao {
+
+	User getUserFromLogin(String username, String password) throws SQLException, UserDataException;
+	User getUserById(int userId) throws SQLException;
+	
+}
