addea1754b3e891c9eea379524c6fca9a7f83ecf
Mario Donchev <mariodonchew@gmail.com>
2018-04-18 18:21:03 +0300

    added new model for gender of user and some fixes of user model

--- FinalProject/src/model/User.java
+++ FinalProject/src/model/User.java
@@ -3,6 +3,8 @@ package model;
 import java.util.List;
 
 import util.BCrypt;
+import util.exceptions.UserDataException;
+import util.validation.UserCredentialsValidation;
 
 public class User {
 
@@ -12,98 +14,51 @@ public class User {
 	private String email;
 	private String address;
 	private String phoneNumber;
-	private String sex;
+	private int gender_id;
 	private boolean isAdmin;
 	
 	private List<Product> favoriteProducts;
 	
 	//constructors
-	public User(String username, String email, String address, String phoneNumber, String sex, boolean isAdmin, List<Product> favoriteProducts) {
+	public User(String username, String password, String email, String address, String phoneNumber, int gender, boolean isAdmin) {
+		
 		this.username = username;
+		this.password = password;
 		this.email = email;
 		this.address = address;
 		this.phoneNumber = phoneNumber;
-		this.sex = sex;
+		this.gender_id = gender;
 		this.isAdmin = isAdmin;
+	}
+
+	public User(int id, String username, String password, String email, String address, String phoneNumber, int gender, boolean isAdmin, List<Product> favoriteProducts, List<Product> shoppingCart){
+		this(username, password, email, address, phoneNumber, gender, isAdmin);
+		this.id = id;
 		this.favoriteProducts = favoriteProducts;
-	}
-	
-	public User(int id, String username, String email, String address, String phoneNumber, String sex, boolean isAdmin, List<Product> favoriteProducts, List<Product> shoppingCart) {
-		this(username, email, address, phoneNumber, sex, isAdmin, favoriteProducts);
-		this.id = id;
-	}
-
-	//getters and setters
-	
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
 	}
 
 	public String getUsername() {
 		return username;
 	}
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
 	public String getEmail() {
 		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
 	}
 
 	public String getAddress() {
 		return address;
 	}
 
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
 
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-
-	public String getSex() {
-		return sex;
-	}
-
-	public void setSex(String sex) {
-		this.sex = sex;
+	public int getGender() {
+		return gender_id;
 	}
 
 	public boolean isAdmin() {
 		return isAdmin;
-	}
-
-	public void setAdmin(boolean isAdmin) {
-		this.isAdmin = isAdmin;
-	}
-
-	public List<Product> getFavoriteProducts() {
-		return favoriteProducts;
-	}
-
-	public void setFavoriteProducts(List<Product> favoriteProducts) {
-		this.favoriteProducts = favoriteProducts;
-	}
-	
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
 	}
 
 	public String hashPassword() {
