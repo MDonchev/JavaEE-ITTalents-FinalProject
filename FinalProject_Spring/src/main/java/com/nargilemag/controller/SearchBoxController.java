42a97191566133ec600db668899a8ac6a2f3a30f
Rangel Angelov <rangelangelov@gmail.com>
2018-05-03 17:21:32 +0300

    added search bar in index page

---
+++ FinalProject_Spring/src/main/java/com/nargilemag/controller/SearchBoxController.java
@@ -0,0 +1,41 @@
+package com.nargilemag.controller;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.nargilemag.model.Product;
+import com.nargilemag.model.dao.ProductDao;
+
+@Controller
+public class SearchBoxController {
+
+	@RequestMapping(value = "/search", method = RequestMethod.POST)
+	public String searchByName(@RequestParam String name, Model model, HttpServletRequest request) {
+		
+		ArrayList<Product> result = new ArrayList<>();
+		try {
+			result = ProductDao.INSTANCE.getProductsWithNameLike(name);
+		} catch (SQLException e) {
+			request.setAttribute("exception", e);
+			return "error";
+		}
+		
+		model.addAttribute("searchResult", result);
+		
+		
+		
+		
+		
+		return "searchResult";
+	}
+	
+	
+}
